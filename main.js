0b300669c5c6611c81de0974190afc1c1df1c367
tai dupree tan <tdupreetan@gmail.com>
2015-02-07 21:16:14 -0500

    minimal website with gimmicky js, yay

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $("#typed").typed({
+    strings: ["@me = <span class=\"symbol\">Person</span>.where(<span class=\"symbol\">name:</span>  <span class=\"string\">\'Tai\'</span>,<br> <span class=\"indent\"><span class=\"symbol\">occupation:</span> <span class =\"string\"> \'programmer\'</span>,</span><br> <span class=\"indent\"><span class=\"symbol\">github:</span> <span class =\"string\"> <a href=\"https://github.com/tdupreetan\">\'/tdupreetan\'</a></span></span><br> <span class=\"indent\">).first</span>"],
+    typeSpeed: 10,
+    backDelay: 600,
+    loop: false,
+    contentType: 'html', // or text
+    // defaults to false for infinite loop
+    loopCount: false
+  });
+});
