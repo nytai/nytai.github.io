ebca10f4dd8017be9c9b25168b7e5536a5f4bbd4
Tai Dupree <tdupreetan@gmail.com>
2015-11-23 14:01:59 -0800

    updates github link

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function(){
 
   $("#typed").typed({
-    strings: ["@me = <span class=\"symbol\">Person</span>.where(<span class=\"symbol\">name:</span>  <span class=\"string\">\'Tai\'</span>,<br> <span class=\"indent\"><span class=\"symbol\">occupation:</span> <span class =\"string\"> \'programmer\'</span>,</span><br> <span class=\"indent\"><span class=\"symbol\">github:</span> <span class =\"string\"> <a href=\"https://github.com/tdupreetan\">\'/tdupreetan\'</a></span></span><br> <span class=\"indent\">).first</span>"],
+    strings: ["@me = <span class=\"symbol\">Person</span>.where(<span class=\"symbol\">name:</span>  <span class=\"string\">\'Tai\'</span>,<br> <span class=\"indent\"><span class=\"symbol\">occupation:</span> <span class =\"string\"> \'programmer\'</span>,</span><br> <span class=\"indent\"><span class=\"symbol\">github:</span> <span class =\"string\"> <a href=\"https://github.com/nytai\">\'/nytai\'</a></span></span><br> <span class=\"indent\">).first</span>"],
     typeSpeed: 10,
     backDelay: 600,
     loop: false,
